42412060b28270f22e2f3e6296ff483932692d95
Jakub Heppner <jakub.heppner@gmail.com>
2024-02-17 10:35:43 +0100

    Dodane zapytania SQL do stworzenia bazy danych

--- data.sql
+++ data.sql
@@ -1,4 +1,30 @@
-CREATE TABLE animal(id BIGINT NOT NULL, name VARCHAR(50), age INT, PRIMARY KEY (id));
-create sequence animal_seq start with 4 increment by 50;
-INSERT INTO animal VALUES (1, 'Reksio', 5), (2, 'Mruczek', 4), (3, 'Benio', 10);
-ALTER TABLE animal ADD COLUMN created_date varchar(50);
+POSTGRES
+
+DROP TABLE IF EXISTS "City";
+DROP TABLE IF EXISTS "CheckWeather";
+
+
+CREATE TABLE City (
+  cityName varchar(25) PRIMARY KEY,
+  countryCode char(2) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE CheckWeather (
+  id serial PRIMARY KEY,
+  cityName varchar(25),
+  countryCode char(2) NOT NULL DEFAULT '',
+  description varchar(45) NOT NULL DEFAULT '',
+  temp real NOT NULL DEFAULT 0,
+  feels_like real DEFAULT 0,
+  temp_min real DEFAULT 0,
+  temp_max real DEFAULT 0,
+  pressure real DEFAULT 0,
+  humidity real DEFAULT 0,
+  windSpeed real DEFAULT 0,
+  clouds_all real DEFAULT 0,
+  forecastTimestamp TIMESTAMP,
+  CONSTRAINT fk_city FOREIGN KEY (cityName) REFERENCES City(cityName)
+);
+
+SELECT * FROM City;
+SELECT * FROM CheckWeather;
